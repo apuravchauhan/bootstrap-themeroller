d5297d49449d0cf1674ee1cb86c6f3570ad20382
Apurav Chauhan <apurav.chauhan@gmail.com>
2012-09-05 23:21:51 +0530

    Making the bootstrap themeroller application source code open to general audience.

---
+++ assets/js/app.js
@@ -0,0 +1,147 @@
+/* ============================================================
+ * app.js v1.0
+ * http://bootstrapthemeroller.com
+ * ============================================================
+ * Copyright Apurav Chauhan, 2012
+ */
+var themeRoller;
+$(function(){
+	//initialize BootstrapThemeroller service
+	themeRoller = new BootstrapThemeroller();
+
+	//register events
+	$('.input-append.color').colorpicker().on('hide', function(ev){
+		 $(this).find('input').change();
+	});
+	$('.input-append.color input').change(themeRoller.updateColorPicker);
+	$('[themeroller="true"]').change(themeRoller.updateTextFields);
+	$('[name="downloadTheme"]').click(themeRoller.downloadTheme);
+
+	// Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
+	$.ajaxTransport('jsonpi', function(opts, originalOptions, jqXHR) {
+	  var url = opts.url;
+	  return {
+		send: function(_, completeCallback) {
+		  var name = 'jQuery_iframe_' + jQuery.now()
+			, iframe, form
+
+		  iframe = $('<iframe>')
+			.attr('name', name)
+			.appendTo('head')
+
+		  form = $('<form>')
+			.attr('method', opts.type) // GET or POST
+			.attr('action', url)
+			.attr('target', name)
+
+		  $.each(opts.params, function(k, v) {
+
+			$('<input>')
+			  .attr('type', 'hidden')
+			  .attr('name', k)
+			  .attr('value', typeof v == 'string' ? v : JSON.stringify(v))
+			  .appendTo(form)
+		  })
+
+		  form.appendTo('body').submit()
+		}
+	  }
+ });
+
+themeRoller.initTheme();
+})//end loader function
+
+/*
+* BootstrapThemeroller service class
+*/
+function BootstrapThemeroller(){
+	/*
+	 *check if less variables are provided in url #, if yes, then reload the style
+	 */
+	this.initTheme = function(){
+		var lessKeyValStringArray = location.hash.substring(1).split('&');
+		var lessKeyValStringArraySize =  lessKeyValStringArray.length;
+
+		for(keyValStrinIndex=0;keyValStrinIndex<lessKeyValStringArraySize;keyValStrinIndex++){
+			lessKeyValPair = lessKeyValStringArray[keyValStrinIndex].split('=');
+			if(lessKeyValPair.length==2){
+				var selector = '[less-var="'+lessKeyValPair[0]+'"]';
+				$(selector).val(lessKeyValPair[1]).change();
+			}
+		}
+	}
+
+	this.getUpdatedLessVars = function(){
+		var finalLessMap = {};
+		$('[themeroller="true"] , .input-append.color input').each(function(index){
+		  var val = $(this).val();
+		  var lessVar = $(this).attr('less-var');
+			 
+		  if($.trim(val)!=''){
+			finalLessMap[lessVar] = val;
+		  }
+		});
+		return finalLessMap;
+	};
+
+	this.updateUrl = function(){
+			var finalLessMap = this.getUpdatedLessVars();
+			var lessString ='';
+			for(key in  finalLessMap){
+				lessString += key+ '='+finalLessMap[key]+'&';
+
+			}
+			location.hash = lessString;
+	};
+	this.downloadTheme = function(){
+			var finalLessMap = themeRoller.getUpdatedLessVars();
+			var css = ["reset.less","scaffolding.less","grid.less","layouts.less","type.less","code.less","labels-badges.less","tables.less","forms.less","buttons.less","sprites.less","button-groups.less","navs.less","navbar.less","breadcrumbs.less","pagination.less","pager.less","thumbnails.less","alerts.less","progress-bars.less","hero-unit.less","tooltip.less","popovers.less","modals.less","dropdowns.less","accordion.less","carousel.less","wells.less","close.less","utilities.less","component-animations.less","responsive-utilities.less","responsive-767px-max.less","responsive-768px-979px.less","responsive-1200px-min.less","responsive-navbar.less"]
+			, js = ["bootstrap-transition.js","bootstrap-modal.js","bootstrap-dropdown.js","bootstrap-scrollspy.js","bootstrap-tab.js","bootstrap-tooltip.js","bootstrap-popover.js","bootstrap-alert.js","bootstrap-button.js","bootstrap-collapse.js","bootstrap-carousel.js","bootstrap-typeahead.js"]
+			, vars = finalLessMap
+			, img = ["glyphicons-halflings.png","glyphicons-halflings-white.png"];
+			
+			
+			$.ajax({
+			type: 'POST'
+		  , url: 'http://bootstrap.herokuapp.com'
+		  , dataType: 'jsonpi'
+		  , params: {
+			  js: js
+			, css: css
+			, vars: vars
+			, img: img
+		  }
+		  })
+		};
+
+	this.updateColorPicker = function(){
+		  var val = $(this).val();
+		  var lessVar = $(this).attr('less-var');
+		 
+		  var parentEle = $(this).parent();
+		  if($.trim(val)==''){
+			val = parentEle.attr('data-color');
+		  }
+			var colorpicker =  parentEle.data('colorpicker');
+			colorpicker.update(val);
+			var lessMap = {};
+			lessMap[lessVar] = val;
+			less.modifyVars(lessMap);
+			themeRoller.updateUrl();
+	  };
+	this.updateTextFields = function(){
+		  var val = $(this).val();
+		  var lessVar = $(this).attr('less-var');
+			 
+		  if($.trim(val)==''){
+			val = $(this).attr('placeholder');
+		  }
+			
+			var lessMap = {};
+			lessMap[lessVar] = val;
+			less.modifyVars(lessMap);
+			themeRoller.updateUrl();
+	  };
+	
+}
+
